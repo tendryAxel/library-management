42d4a7cb4c0c21863cafef647cb0b2024ffe3ed0
tendryAxel <hei.tendry@gmail.com>
2023-11-15 19:33:52 +0300

    feat : test connection with local database

--- src/LibraryManagementApplication.java
+++ src/LibraryManagementApplication.java
@@ -1,4 +1,7 @@
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class LibraryManagementApplication {
     public static void main(String[] args) {
@@ -6,5 +9,23 @@ public class LibraryManagementApplication {
         String password = java.lang.System.getenv("PG_PASSWORD");
         String user = java.lang.System.getenv("PG_USER");
         String url = java.lang.System.getenv("PG_URL");
+
+        // Connect
+        try {
+            Connection connection = DriverManager.getConnection(
+                    url,
+                    user,
+                    password
+            );
+            ResultSet result = connection.prepareStatement(
+                    "select * from test;"
+            ).executeQuery();
+            while (result.next()){
+                System.out.println(result.getInt("id"));
+            }
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

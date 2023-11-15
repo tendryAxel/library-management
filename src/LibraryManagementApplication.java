1a01657be1af043d5f0415105d48b8e432a896a7
tendryAxel <hei.tendry@gmail.com>
2023-11-15 19:24:50 +0300

    feat : test all environmental variable

--- src/LibraryManagementApplication.java
+++ src/LibraryManagementApplication.java
@@ -1,6 +1,10 @@
 public class LibraryManagementApplication {
     public static void main(String[] args) {
         String password = java.lang.System.getenv("PG_PASSWORD");
+        String user = java.lang.System.getenv("PG_USER");
+        String url = java.lang.System.getenv("PG_URL");
         System.out.println(password);
+        System.out.println(user);
+        System.out.println(url);
     }
 }

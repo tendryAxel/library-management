4977dd50626e0d61e33b2f88aaa6df4f7205a635
tendryAxel <hei.tendry@gmail.com>
2023-11-15 19:28:52 +0300

    fix : instruction of environmental variable in README.md

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ API that manages library.
 
 ## Link petstore
 https://petstore.swagger.io/?url=https://raw.githubusercontent.com/tendryAxel/library-management/oas-td4-std22075-std21089/docs/api.yml
+
+## To run
+Add environmental variable:
+- PG_PASSWORD : password of postgres user
+- PG_USER : the postgres user
+- PG_URL : the url of the database
--- src/LibraryManagementApplication.java
+++ src/LibraryManagementApplication.java
@@ -1,10 +1,10 @@
+import java.sql.DriverManager;
+
 public class LibraryManagementApplication {
     public static void main(String[] args) {
+        // Importation of variable
         String password = java.lang.System.getenv("PG_PASSWORD");
         String user = java.lang.System.getenv("PG_USER");
         String url = java.lang.System.getenv("PG_URL");
-        System.out.println(password);
-        System.out.println(user);
-        System.out.println(url);
     }
 }

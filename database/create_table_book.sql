f7955ce681d0b8750bf8dff78497ecac6cd1e41d
tendryAxel <hei.tendry@gmail.com>
2023-11-16 15:45:22 +0300

    feat : script of creation of book table, with moch data

---
+++ database/create_table_book.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS "book" (
+    id VARCHAR(50),
+    bookname VARCHAR(150),
+    pagenumbers INT,
+    realeasedate DATE,
+    topics VARCHAR(20) CHECk (topics IN (
+        'ROMANCE', 'COMEDY', 'OTHER'
+    ))
+);
+INSERT INTO "book"
+VALUES(
+        '1',
+        'Harry Potter a l''ecole des sorcier',
+        150,
+        '1997-06-26',
+        'OTHER'
+     ),(
+        '2',
+        'Le Seigneur des Anneaux:La Communaute de l''Anneau',
+        250,
+        '1954-07-29',
+        'OTHER'
+     ),(
+        '3',
+        'Da Vinci Code',
+        50,
+        '2003-03-18',
+        'OTHER'
+)
